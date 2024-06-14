3253c05a7a959c072fc191db085a246a56642183
haidwgunsw <haiduong.unsw@gmail.com>
2024-06-14 23:41:41 +1000

    Binary search day 4

---
+++ 34_Find First and Last Position of Element/solution.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left = self.binarySearch(nums,target,True)
+        right = self.binarySearch(nums,target,False)
+        return [left,right]
+
+    
+    def binarySearch(self,nums,target,leftBias):
+        left = 0
+        right = len(nums) - 1
+        i = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                i = mid
+                if leftBias:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return i
+        
+        
